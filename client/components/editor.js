4e50dba325428d7fdb68880164d741550d157480
Calvin Tan <Calvin@Wiings-MacBook-Pro-2.local>
2016-01-11 17:32:49 +0800

    Added code editor feature

---
+++ client/components/editor.js
@@ -0,0 +1,42 @@
+Template.editor.helpers({
+  docid: function(){
+    var doc = Documents.findOne();
+    if (doc){
+      return doc._id
+    } else {
+      return undefined
+    }
+  },
+  config: function(){
+    return function(editor){
+      // console.log(editor)
+      editor.on("change", function(cm_editor, info){
+        // console.log(cm_editor.getValue())
+        $("#viewer_iframe").contents().find("html").html(cm_editor.getValue())
+        Meteor.call("addEditingUser")
+      })
+    }
+  }
+});
+
+Template.editingUsers.helpers({
+  users: function() {
+    var doc, eusers, users;
+    doc = Documents.findOne()
+    if (!doc){
+      return
+    }
+    eusers = EditingUsers.findOne()
+    if (!eusers){
+      return
+    }
+
+    users = []
+    var i = 0;
+    for (var user_id in eusers.users) {
+      users[i] = eusers.users[user_id]
+      i++
+    }
+    return users;
+  }
+})
--- server/server.js
+++ server/server.js
@@ -44,5 +44,27 @@ Meteor.methods({
       }
       Documents.upsert({ _id: docId }, obj)
     })
+  },
+  addEditingUser: function(){
+    var doc, user, eusers
+    doc = Documents.findOne()
+    if (!doc){
+      return
+    }
+    if (!this.userId){
+      return
+    }
+    user = Meteor.user().profile
+    eusers = EditingUsers.findOne({docid:doc._id})
+    if (!eusers) {
+      eusers = {
+        docid: doc._id,
+        users: {}
+      }
+    }
+    user.lastEdit = new Date()
+    eusers.users[this.userId] = user
+
+    EditingUsers.upsert({_id:eusers._id}, eusers)
   }
 })
