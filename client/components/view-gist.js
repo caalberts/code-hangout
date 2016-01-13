1676a8de587c686a54e6d021d52886a1464dfae4
Albert <albertlimca@gmail.com>
2016-01-13 10:16:41 +0800

    bug fix

--- client/components/view-gist.js
+++ client/components/view-gist.js
@@ -2,5 +2,7 @@ Meteor.subscribe('user')
 Meteor.subscribe('files')
 
 Template.viewGist.onCreated(function () {
-  Session.set('gistId', this.data.gistId)
+  if (Meteor.user()) {
+    Session.set('gistId', this.data.gistId)
+  }
 })
