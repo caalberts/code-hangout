2c2b5ae2d2a4778654bf647df8446a6ae0c5865c
Albert <albertlimca@gmail.com>
2016-01-13 23:05:34 +0800

    changes to file list display

--- client/components/view-files.js
+++ client/components/view-files.js
@@ -1,6 +1,11 @@
 Meteor.subscribe('files')
 
-Template.viewFiles.helpers({
+Template.fileList.onCreated(function () {
+  const file = Files.findOne({ gistId: Session.get('gistId') })
+  Session.set('fileId', file._id)
+})
+
+Template.fileList.helpers({
   files: function () {
     return Files.find({ gistId: this.gistId })
   },
@@ -8,7 +13,8 @@ Template.viewFiles.helpers({
     return !(this.ownerId === Meteor.userId())
   }
 })
-Template.viewFiles.events({
+
+Template.fileList.events({
   'submit form': function (event) {
     event.preventDefault()
     const updateContent = {
@@ -17,5 +23,12 @@ Template.viewFiles.events({
     updateContent.files[this.filename] = { content: event.target.fileContent.value }
 
     Meteor.call('updateGist', this.gistId, this.filename, updateContent)
+  }
+})
+
+Template.fileItem.events({
+  'click a.file': function (event) {
+    // load content in editor
+    Session.set('fileId', this._id)
   }
 })
